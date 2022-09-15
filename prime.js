c84b08a86a1a52f51111856fe79ae40ab61e0d73
Neha <nehasahu2227@gmail.com>
2022-09-15 16:39:17 +0530

    code fixed, please check

--- prime.js
+++ prime.js
@@ -1,10 +1,10 @@
 let num=13;
 let count=0;
     for (i=1;i<=num;i++){
-        if(num/i==0){
+        if(num%i==0){
             count++;
         }
-    }if(count==1){
+    }if(count==2){
         console.log("Yes");
     }else{
         console.log("No");
